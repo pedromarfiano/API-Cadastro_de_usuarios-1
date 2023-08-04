74f512055d599539f1368a226f399fe90558b2ff
marfian609 <pedromarfiano@gmail.com>
2023-08-04 13:00:56 -0300

    inplementacao delete

--- src/server/controllers/DeletePerson.ts
+++ src/server/controllers/DeletePerson.ts
@@ -6,15 +6,15 @@ import person from "../database";
 const deletePerson = async (req:Request, res:Response) => {
     const { id } = req.params
 
-    const user = person.findById(id);
-
-    if(user[0]){
-        return res.status(200).json(user);
-    } else{
+    person.deleteOne({_id: id}).then(() =>{
+        return res.status(200).json({
+            "message": "Usuario deletado"
+        })
+    }).catch(() =>{
         return res.status(404).json({
             "erro": "id invalido"
         })
-    }
+    })
 }
 
 export default deletePerson
